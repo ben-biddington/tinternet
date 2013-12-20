fa16015f976bacaa6fa88a1b8cb1b3ed138ec158
Ben Biddington <ben.biddington@xero.com>
2013-12-20 15:06:18 +1300

    Moved adapter class => adapters/

--- lib/t.rb
+++ lib/t.rb
@@ -1,3 +1,5 @@
 dir = File.join(File.dirname(__FILE__))
 
+$LOAD_PATH.unshift dir
+
 Dir.glob(File.join(dir, "t", "**", "*.rb")).each {|f| require f}
---
+++ lib/t/adapters/rest_client_internet.rb
@@ -0,0 +1,70 @@
+class RestClientInternet
+  require "rest_client"
+  require "audible"; include Audible
+
+  def initialize(opts ={})
+    @ssl_client_cert = opts[:ssl_client_cert]
+    @proxy           = opts[:proxy]
+  end
+
+  def execute(request); try_execute request; end
+
+  private
+    
+  def try_execute(request)
+    begin
+      execute_core request
+    rescue RestClient::RequestTimeout => t
+      timeout t
+    rescue RestClient::Exception => e
+      non_20x e
+    end
+  end
+
+  def non_20x(e)
+    T::Response.new(e.response.code, e.response.headers, e.response.body)
+  end
+
+  def timeout(error)
+    T::Response.new(-1, {}, "The request timed out after #{request_timeout_in_seconds}s.")
+  end
+
+  def execute_core(request)
+    apply_proxy if proxy?
+    
+    notify_requesting request
+  
+    reply = RestClient::Request.execute(opts_for(request))
+
+    T::Response.new(200, reply.headers, reply)
+  end
+  
+  def opts_for(request)
+    {
+      :method  => request.verb, 
+      :url     => request.uri.to_s, 
+      :headers => request.headers, 
+      :payload => request.body, 
+      :timeout => request_timeout_in_seconds
+    }.merge ssl_opts
+  end
+
+  def request_timeout_in_seconds; 30; end
+
+  def ssl_opts
+    return {} unless ssl_client_cert?
+    {
+      :ssl_client_cert => OpenSSL::X509::Certificate.new(File.read(@ssl_client_cert.cert)),
+      :ssl_client_key  => OpenSSL::PKey::RSA.new(File.read(@ssl_client_cert.key)),
+      :verify_ssl      => @ssl_client_cert.verify_ssl
+    }
+  end
+
+  def apply_proxy; RestClient.proxy = @proxy; end
+  def ssl_client_cert?; @ssl_client_cert; end
+  def proxy?; @proxy; end
+
+  def notify_requesting(request)
+    notify :requesting, request
+  end
+end
