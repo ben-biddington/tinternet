a6238803fd3a226c4f7e2177514ecde6046e97d4
Ben Biddington <ben.biddington@xero.com>
2014-12-08 13:26:42 +1300

    Refactor: renamed to match <https://www.ietf.org/rfc/rfc2388.txt>

--- lib/tinternet/payload/www_form_url_encoded.rb
+++ lib/tinternet/payload/www_form_url_encoded.rb
@@ -1,23 +1,21 @@
 module T
-  class XWwwFormUrlencoded
-    def initialize(parameters={})
-      @parameters = parameters
+  class XWwwFormUrlencoded # https://www.ietf.org/rfc/rfc2388.txt
+    def initialize(fields={})
+      @fields = fields
     end
     
     def to_s
-      result = []
-      
-      @parameters.each_pair do |name, value|
-        result << "#{encode(name)}=#{encode(value)}"
-      end
-      
-      result.join "&"
+      [].tap do |result|
+        @fields.each_pair do |name, value|
+          result << "#{encode(name)}=#{encode(value)}"
+        end
+      end.join "&"
     end
     
     private
     
     def encode(what)
-     URI.escape(what)
+      URI.escape(what)
     end
   end
 end
